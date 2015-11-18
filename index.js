8a5339acdd4684726e9f17500053391ad2f6517f
queicherius <queicherius@gmx.de>
2015-11-18 23:07:32 +0100

    Rewritten & cleaned up player engine - use "src" instead of child elements

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* global progressHandler, changeHandler, items */
 var EventEmitter = require('events').EventEmitter
 var inherits = require('inherits')
 
@@ -6,81 +5,88 @@ module.exports = PlayerEngine
 inherits(PlayerEngine, EventEmitter)
 
 function PlayerEngine (files, opts) {
-  if (!(this instanceof PlayerEngine)) return new PlayerEngine(files, opts)
-  // needs to be added to the UI
-  this.engine = document.createElement('AUDIO')
-  this.files = files
-  // example of emitting an event with an object as payload (payload optional)
-  // this.emit('eventName', { my: 'payload' })
-  this.engine.addEventListener('timeupdate', () => this.emit('progress')) // event: this.engine.timeupdate, TODO: add payload when necessary
-  this.engine.addEventListener('ended', () => this.emit('changeHandler')) // event: this.engine.ended TODO: add payload when necessary
-}
-
-// Set a random track as source
-function getRandomTrack () {
-  var src = document.createElement('SOURCE')
-  var file = this.files[Math.floor(Math.random() * items.length)] // items comes from where?
-  src.setAttribute('src', file.toURL())
-  src.setAttribute('type', 'audio/mp3')
-  this.engine.appendChild(src)
-}
-
-// Removes the current source
-PlayerEngine.prototype.clear = function () {
-  while (this.engine.firstChild) {
-    this.engine.removeChild(this.engine.firstChild)
+  if (!(this instanceof PlayerEngine)) {
+    return new PlayerEngine(files, opts)
   }
+
+  // Save the files into an internal handling
+  this.files = files
+
+  // Add an audio element as the actual "play" engine
+  this.engine = document.createElement('audio')
+
+  // Audio element events
+  var self = this
+
+  this.engine.addEventListener('timeupdate', function () {
+    self.emit('progress', self.engine.currentTime)
+  })
+
+  this.engine.addEventListener('ended', function () {
+    self.skip()
+  })
 }
 
-// Return the URL of the current track when the player is active else it returns null
+// All outside event listeners are set up, we can setup things internally that throw events
+PlayerEngine.prototype.start = function () {
+  this.setNextTrack()
+}
+
+// Set the next track to play
+PlayerEngine.prototype.setNextTrack = function () {
+  var file = this.files[Math.floor(Math.random() * this.files.length)]
+  this.engine.src = file.toURL()
+
+  // Publish new track to the outside world
+  var self = this
+  this.engine.addEventListener('loadedmetadata', function () {
+    self.emit('songState', {
+      name: file.name,
+      duration: self.engine.duration
+    })
+  })
+}
+
+// Return the URL of the current track when the player is active else return "false"
 PlayerEngine.prototype.playing = function () {
-  if (this.engine.paused === true) return null
-  return this.engine.currtSrc
+  return this.engine.paused === true ? false : this.engine.currentSrc
 }
 
 // Toggles the state of the player to playing/pause corresponding to the current state
 PlayerEngine.prototype.toggle = function () {
-  if (this.engine.paused === true) {
-    if (this.engine.childNodes === '') getRandomTrack()
-    this.engine.play()
-    return
-  }
-  this.engine.pause()
+  this.engine.paused === true ? this.play() : this.pause()
 }
 
 // Start the audio playback
 PlayerEngine.prototype.play = function () {
-  if (this.engine.childNodes === '') getRandomTrack()
   this.engine.play()
+  this.emit('playingState', true)
 }
 
-// Stop the audio playback, resets the PlayerEngine to the beginnig of the track
+// Stop the audio playback, an resets to the beginning of the track
 PlayerEngine.prototype.stop = function () {
-  this.engine.pause()
+  this.pause()
   this.engine.currentTime = 0
 }
 
 // Pauses the audio playback, can be resumed from the current state
 PlayerEngine.prototype.pause = function () {
   this.engine.pause()
+  this.emit('playingState', false)
 }
 
 // Skips the current song
 PlayerEngine.prototype.skip = function () {
-  // Four steps: 1. Pause, 2. Clear the Player 3. Set new Track 4. Play
-  this.engine.pause()
-  while (this.engine.firstChild) {
-    this.engine.removeChild(this.engine.firstChild)
-  }
-  if (this.engine.childNodes === '') getRandomTrack()
-  this.engine.play()
-  this.emit('change')
+  this.setNextTrack()
+  this.play()
 }
 
 // Sets volume to the given volume, if volume is empty act as get function
 PlayerEngine.prototype.volume = function (volume) {
-  if (volume === 'undefined' || volume === 'null') return this.engine.volume
-  this.engine.volume = volume
+  if (volume) {
+    this.engine.volume = volume
+  }
+  return this.engine.volume
 }
 
 // Sets the playback position
